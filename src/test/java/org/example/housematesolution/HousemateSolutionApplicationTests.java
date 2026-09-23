d28f977041bec2da631a210e29d843a6875c2c4e
ineoaas <justinleomaas@gmail.com>
2026-09-24 00:24:17 +0200

    Replaced Testcontainers with a real MySQL service for tests

--- src/test/java/org/example/housematesolution/HousemateSolutionApplicationTests.java
+++ src/test/java/org/example/housematesolution/HousemateSolutionApplicationTests.java
@@ -2,10 +2,8 @@ package org.example.housematesolution;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 @SpringBootTest
-@Import(TestcontainersConfiguration.class)
 class HousemateSolutionApplicationTests {
 
     @Test
